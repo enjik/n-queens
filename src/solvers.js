33747f4337680c1d80e2b155f39cfb6622786af2
enjik <enjikim17@gmail.com>
2018-06-21 17:18:43 -0700

    Implement countNRooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,22 +14,65 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
+// findNRooksSolution(): returns a single solution to the n-rooks problem
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = undefined;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
+    /*
+    inputs: n (size of nxn board)
+    outputs: board where rooks are all safe 
+    constraints: n cannot be negative or zero, cannot be too big (bc of runtime)
+    edge cases: 
+    This fn should: find one solution where n rooks on a n x n board are safe from each other
+    Relationship btwn inputs and outputs: n rooks, n is the grid of board
+    
+    array solutions
+    function should be inner-recursive (current board, number of rooks placed on board so far)
+    base case: number of rooks = n    
+          push in board to solutions array
+    if(placing rook does not have a rook conflict)
+      place a rook on empty unit
+    run recursive function(current board, number of rooks++) 
+    
+    
+    
+    */
+
+
+// countNRooksSolutions(): returns a count of the total number of solutions to the n-rooks problem
+
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  var solutionCount = 0; 
+       // counting up rows from zero to n-1
+      // base case: when row = n -1
+     // increment count
+  var board = new Board({n:n});
+      // recurse when rows are less than n - 1
+  var solve = function(row) {
+    if(row === n) {
+      solutionCount++;
+      return;
+    } else {
+      for(var i = 0; i < n; i++) {
+        board.togglePiece(row, i);
+        if(!board.hasAnyRooksConflicts()) {
+          solve(row + 1);
+        } 
+         board.togglePiece(row, i);      
+      }
+    }
+  }
+  solve(0);
   return solutionCount;
 };
 
+
+// findNQueensSolution(): returns a single solution to the n-queens problem
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
@@ -37,7 +80,7 @@ window.findNQueensSolution = function(n) {
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
-
+//countNQueensSolutions(): returns a count of the total number of solutions to the n-queens problem
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
@@ -45,3 +88,20 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+
+
+
+
+/*
+    inputs: 
+    outputs: 
+    constraints: 
+    edge cases:
+    This fn should: 
+    Relationship btwn inputs and outputs: 
+    
+    
+    */
